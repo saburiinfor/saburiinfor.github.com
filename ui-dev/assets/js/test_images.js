3b7ddbdbeb744a21924cae8d41d342b4c9e90e54
Ravi Kiran Lingam <lravikiran@icloud.com>
2019-06-18 17:05:44 +0530

    Few changes in ajax and images file

--- ui-dev/assets/js/test_images.js
+++ ui-dev/assets/js/test_images.js
@@ -1,5 +1,8 @@
-// {
-// "id1":{" im":<img src="https://cdn-images-1.medium.com/max/1200/1*_RQU7TYtrWBNqXWAnthU0Q.png">},
-//
-// "id2":{" im":<img src="https://cdn-images-1.medium.com/max/1200/1*_RQU7TYtrWBNqXWAnthU0Q.png">}
-// }
+{
+  id1: {
+    im: '../images/ajax.png'
+  }
+  id2: {
+    im: '../threeMuskteers.jpg'
+  }
+}
