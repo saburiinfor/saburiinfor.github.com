bf4b60d4cab275384afa628fbec8c1fa327c06ca
Ravi Kiran Lingam <lravikiran@icloud.com>
2019-06-18 15:38:48 +0530

    adding JS folder

---
+++ ui-dev/assets/js/test_images.js
@@ -0,0 +1,5 @@
+// {
+// "id1":{" im":<img src="https://cdn-images-1.medium.com/max/1200/1*_RQU7TYtrWBNqXWAnthU0Q.png">},
+//
+// "id2":{" im":<img src="https://cdn-images-1.medium.com/max/1200/1*_RQU7TYtrWBNqXWAnthU0Q.png">}
+// }
