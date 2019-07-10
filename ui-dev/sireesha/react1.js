560612c8893905669d081ef602308f07bc1972c8
sireesha-kampa <52244090+sireesha-kampa@users.noreply.github.com>
2019-07-10 14:02:20 +0530

    Add files via upload

---
+++ ui-dev/sireesha/react1.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import './Header.css1';
+
+class Header extends Component {
+    
+    constructor(){
+        super()
+
+        this.state={
+            title:'React App'
+        }
+        
+    }
+
+    inputChange(event){
+        console.log(event.target.value)
+    }
+
+    render(){
+        return(
+            <header>
+                <div className="logo"
+                onClick={()=>{console.log('header clicked')}}>
+
+                {this.state.title}
+                
+                </div>
+                <center>
+                    <input onChange={this.inputChange}/>
+                </center>
+                <p>User text Here</p>
+            </header>
+        )
+    }
+}
+
+export default Header;
+
